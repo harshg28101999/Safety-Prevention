dba9212ac823228c64481dcc0485bf70e508331b
harshg28101999 <hmg5265@psu.edu>
2023-10-17 21:52:21 -0400

    Added TODO text file

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -3,20 +3,26 @@ import { View, Text, StyleSheet } from 'react-native';
 import Button from '../components/Button';
 import TextInputField from '../components/TextInputField';
 
-const SignupScreen = () => {
+const SignupScreen = ({ navigation}) => {
   const [username, setUsername] = useState('');
+  const [firstname, setfirstname] = useState('');
+  const [lastname, setlastname] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
 
   const handleSignup = () => {
     // Logic to handle signup
+    navigation.navigate('Home');
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Signup</Text>
+      <TextInputField placeholder="First Name" value={firstname} onChangeText={setfirstname} />
+      <TextInputField placeholder="Last Name" value={lastname} onChangeText={setlastname} />
       <TextInputField placeholder="Email" value={email} onChangeText={setEmail} />
-      <TextInputField placeholder="Username" value={username} onChangeText={setUsername} />
+
+      <TextInputField placeholder="Username" value={username} onChangeText={setUsername} secureTextEntry/>
       <TextInputField placeholder="Password" value={password} onChangeText={setPassword} secureTextEntry />
       <Button title="Signup" onPress={handleSignup} />
     </View>
